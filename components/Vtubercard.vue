a0967aeddf3ade3aa7a454689b3b254e38f54239
Iroha71 <y.koba.h1071@outlook.jp>
2020-08-14 00:24:59 +0900

    🔼コンポーネントの汎用化 #1

--- components/Vtubercard.vue
+++ components/Vtubercard.vue
@@ -1,24 +1,81 @@
 <template>
-    <div class="card column is-2 is-12-tablet-only">
+<transition mode="out-in" name="bounce">
+<div class="card column is-2 is-12-tablet-only" v-if="isSearching()">
   <div class="card-image">
     <figure class="image is-1by1">
-      <img src="https://s3-ap-northeast-1.amazonaws.com/liver-icons/400x400/Tsukino_Mito.png" alt="Placeholder image">
+      <img :src="image" alt="Placeholder image">
     </figure>
   </div>
   <div class="card-content">
     <div class="media">
       <div class="media-content">
-        <p class="title is-4">月ノ美兎</p>
-        <p class="subtitle is-6">@MitoTsukino</p>
+        <p class="title is-4">{{ name }}</p>
+        <p class="subtitle is-6">{{ twitter }}</p>
+        <div v-for="z in zokusei" :key="z">
+          <b-tag type="is-primary" is-medium rounded>{{ z }}</b-tag>
+        </div>
       </div>
     </div>
-
   </div>
 </div>
+</transition>
 </template>
 
-<style lang="scss" scoped>
-    .card{
-        min-width: 194px;
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    twitter: {
+      type: String
+    },
+    image: {
+      type: String
+    },
+    zokusei: {
+      type: Array
+    },
+    searchName: {
+      type: String,
+      default: ''
     }
+  },
+
+  methods: {
+    isSearching:function() {
+      if (this.searchName === '' || this.searchName === this.name) {
+        return true
+      } else if (this.searchName != this.name) {
+        return false
+      }
+    }
+  }
+  // props: ['name', ]
+}
+</script>
+
+<style lang="scss" scoped>
+.card{
+    min-width: 194px;
+}
+
+.bounce-enter-active {
+  animation: bounce-in .5s;
+}
+.bounce-leave-active {
+  animation: bounce-in .5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
 </style>
